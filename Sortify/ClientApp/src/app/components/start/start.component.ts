1043814a99aa0e3eb82da0f4795cd26d7a53c7ef
Tomasz Fijałkowski <tomelecf@gmail.com>
2020-09-05 02:15:47 +0200

    Add start component

---
+++ Sortify/ClientApp/src/app/components/start/start.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.sass']
+})
+export class StartComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
