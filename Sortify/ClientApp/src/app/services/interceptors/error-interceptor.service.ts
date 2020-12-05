57b055c7759e8e8d1862fac0b685d9cd87c6427f
Tomasz Fijałkowski <tomelecf@gmail.com>
2020-12-05 14:30:56 +0100

    Migrate application to Angular 11

--- Sortify/ClientApp/src/app/services/interceptors/error-interceptor.service.ts
+++ Sortify/ClientApp/src/app/services/interceptors/error-interceptor.service.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
