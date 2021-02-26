8ebc09ddc7912c70974c0bd763066040e536e7c1
Tomasz Fijałkowski <tomelecf@gmail.com>
2021-02-26 19:04:46 +0100

    Cache user details to avoid unnecessary requests

--- Sortify/ClientApp/src/app/services/user.service.ts
+++ Sortify/ClientApp/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { HttpClient } from '@angular/common/http';
@@ -12,16 +12,22 @@ import { OperationResult } from '../models/operation-result';
 })
 export class UserService {
   private userDetails: UserDetails;
+  private cachedResponse: OperationResult<GetUserDetailsResponse>;
 
   constructor(private http: HttpClient,
     @Inject('BASE_URL') private baseUrl: string) {
   }
 
   getUserDetails(): Observable<OperationResult<GetUserDetailsResponse>> {
+    if (this.cachedResponse) {
+      return of(this.cachedResponse);
+    }
+
     return this.http.get<OperationResult<GetUserDetailsResponse>>(this.baseUrl + 'user').pipe(
       map((response: OperationResult<GetUserDetailsResponse>) => {
         if (response.successful) {
           this.userDetails = response.result.userDetails;
+          this.cachedResponse = response;
         }
         return response;
       })
